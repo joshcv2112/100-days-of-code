75b828e585043f7f63bd9f9f14ba2a9b6c56d62e
Joshua Vaughan <joshcv2112@gmail.com>
2017-06-04 11:46:34 -0600

    Setup input validation in makeSelection() function

--- src/Fraction Calculator/Interface.cpp
+++ src/Fraction Calculator/Interface.cpp
@@ -9,8 +9,19 @@ Interface::Interface()
 int Interface::makeSelection()
 {
 	int selection;
-	std::cout << "Make a selection\n1. Whole numbers\n2.Fractions" << std::endl;
+	std::cout << "Make a selection\n1. Whole numbers\n2. Fractions" << std::endl;
 	std::cin >> selection;
+
+	// Validates input to be an int that is either 1 or 2.
+	// Repeats if input is invalid.
+	while (std::cin.fail() || (selection < 1 || selection > 2))
+	{
+		std::cout << "Invalid input. Try again . . ." << std::endl;
+		std::cin.clear();
+		std::cin.ignore(256, '\n');
+		std::cin >> selection;
+	}
+
 	return selection;
 }
 
--- src/Fraction Calculator/main.cpp
+++ src/Fraction Calculator/main.cpp
@@ -3,23 +3,8 @@
 
 int main()
 {
-	bool repeat = true;
-	while (repeat)
-	{
-		int selection = Interface::makeSelection();
-		if (selection == 1)
-		{
-			Interface::wholeNumbers();
-		}
-		else if (selection == 2)
-		{
-			Interface::fractions();
-		}
-		else
-			std::cout << "Invalid selection, try again . . ." << std::endl;
-	}
+	int selection = Interface::makeSelection();
 	
-	std::cout << "Terminating program . . ." << std::endl;
 
 	return 0;
 }
